2f72a60ba828fbb060cb95a5d6c0ab83603368e8
Kamran Zafar <kamran.zafar@cognitiveaxis.com>
2016-02-12 17:30:24 +0500

    Removing unsed JS and CSS files. Adding custom Slideshow block code.

---
+++ modules/barney_river_utilities/src/Plugin/Block/HomepageSlideshowBlock.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\barney_river_utilities\Plugin\Block\HomepageSlideshowBlock.
+ */
+namespace Drupal\barney_river_utilities\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\image\Entity\ImageStyle;
+
+
+
+/**
+ * Provides a 'Homepage Slideshow' Block
+ *
+ * @Block(
+ *   id = "homepageslideshow",
+ *   admin_label = @Translation("Homepage Slideshow"),
+ * )
+ */
+
+
+class HomepageSlideshowBlock extends BlockBase {
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+    $query = \Drupal::entityQuery('node')
+        ->condition('status', 1)
+        ->condition('type', 'slideshow_slide')
+        ->sort('field_order', 'ASC');
+    $nids = $query->execute();
+    $nodes = node_load_multiple($nids);
+
+    $ind = 0;
+    $output_slide = $output_slide_bullet = $output = '';
+    foreach($nodes as $node){
+        $class = ($ind==0) ? 'active' : '';
+        $output_slide_bullet .= '<li data-target="#myCarousel" data-slide-to="' . $ind . '" class="' . $class . '"></li>';
+        $path = $node->field_image->entity->url();
+        //$url = ImageStyle::load('')->buildUrl($path);
+        $output_slide .= '<div class="item ' . $class . '">
+                            <img src="' . $path . '" alt="">
+                          </div>';
+        $ind++;
+    }
+    $output = '<div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                  ' . $output_slide_bullet . '
+                </ol>
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
+                  ' . $output_slide . '
+                </div>
+
+                <!-- Left and right controls -->
+                <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                  <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                  <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                  <span class="sr-only">Next</span>
+                </a>
+              </div>';
+
+        return array(
+          '#type' => 'markup',
+          '#markup' => $output,
+        );
+      }
+}
+?>
