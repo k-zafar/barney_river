ef849915a35f2919cdfdaf64f2820439bbe02b8a
mainulhassan-ca <mainul.hassan@cognitiveaxis.com>
2016-02-17 18:21:39 +0500

    About page tab setting for mobile

--- modules/barney_river_utilities/src/Plugin/Block/TeamMemberBlock.php
+++ modules/barney_river_utilities/src/Plugin/Block/TeamMemberBlock.php
@@ -36,13 +36,13 @@ class TeamMemberBlock extends BlockBase {
     $nodes = node_load_multiple($nids);
     //$nodes = entity_load_multiple('node', $nids);
     $ind = 1;
-    $output = '<div class="tabbable tabs-left">
+    $output = '<div class="tabbable tabs-left tabcordion">
                 <ul class="nav nav-tabs">';
-    foreach($nodes as $node){
+    foreach($nodes as $node){     
               $class = ($ind==1) ? 'active' : '';
 
-              $output .= '<li class="' . $class . '"><a href="#team_member_tab' . $ind . '" data-toggle="tab">' . $node->title->value . '<span>' . $node->get('field_designation')->value . '</span></a></li>';
-              $ind++;
+              $output .= '<li class="' . $class . '"><a data-target="#team_member_tab' . $ind . '" data-toggle="tab">' . $node->title->value . '<span>' . $node->get('field_designation')->value . '</span></a></li>';
+              $ind++;                                
     }
 
     $ind = 1;
@@ -62,34 +62,6 @@ class TeamMemberBlock extends BlockBase {
             }
             $output .= '</div>
           </div>';
-
-          $output_temp = '<div class="tabcordion">
-  <ul class="nav nav-tabs">
-    <li class="active"><a data-target=".home">Home</a></li>
-    <li><a data-target=".profile">Profile</a></li>
-    <li><a data-target=".messages">Messages</a></li>
-    <li><a data-target=".settings">Settings</a></li>
-  </ul>
-  <div class="tab-content">
-    <div class="home active in">
-      <h3>Home</h3>
-      <p>Rhoncus magna nec enim, et proin aliquet mid, porta magnis.</p>
-    </div>
-    <div class="profile">
-      <h3>Profile</h3>
-      <p>Odio mattis, non ut! Porttitor nunc phasellus cras elementum.</p>
-    </div>
-    <div class="messages">
-      <h3>Messages</h3>
-      <p>Enim hac tristique elementum, nec rhoncus porttitor sagittis cum.</p>
-    </div>
-    <div class="settings">
-      <h4>Settings</h4>
-      <p>Arcu auctor, porttitor tincidunt, aliquam ut ut, placerat porta pulvinar dictumst?</p>
-    </div>
-  </div>
-</div>';
-
         return array(
           '#type' => 'markup',
           '#markup' => $output,
