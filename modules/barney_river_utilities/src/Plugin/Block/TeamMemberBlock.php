8195a8cfe6ee922c28be62736dd02fe83c4f5c76
Kamran Zafar <kamran.zafar@cognitiveaxis.com>
2016-02-17 17:11:38 +0500

    Updating code for Team Member responsive design.

--- modules/barney_river_utilities/src/Plugin/Block/TeamMemberBlock.php
+++ modules/barney_river_utilities/src/Plugin/Block/TeamMemberBlock.php
@@ -63,9 +63,42 @@ class TeamMemberBlock extends BlockBase {
             $output .= '</div>
           </div>';
 
+          $output = '<div class="tabcordion">
+  <ul class="nav nav-tabs">
+    <li class="active"><a data-target=".home">Home</a></li>
+    <li><a data-target=".profile">Profile</a></li>
+    <li><a data-target=".messages">Messages</a></li>
+    <li><a data-target=".settings">Settings</a></li>
+  </ul>
+  <div class="tab-content">
+    <div class="home active in">
+      <h3>Home</h3>
+      <p>Rhoncus magna nec enim, et proin aliquet mid, porta magnis.</p>
+    </div>
+    <div class="profile">
+      <h3>Profile</h3>
+      <p>Odio mattis, non ut! Porttitor nunc phasellus cras elementum.</p>
+    </div>
+    <div class="messages">
+      <h3>Messages</h3>
+      <p>Enim hac tristique elementum, nec rhoncus porttitor sagittis cum.</p>
+    </div>
+    <div class="settings">
+      <h4>Settings</h4>
+      <p>Arcu auctor, porttitor tincidunt, aliquam ut ut, placerat porta pulvinar dictumst?</p>
+    </div>
+  </div>
+</div>';
+
         return array(
           '#type' => 'markup',
           '#markup' => $output,
+          '#attached' => array(
+              'library' =>  array(
+                 'barney_river_utilities/tabcordion',
+                 'barney_river_utilities/tabcordion_hook',
+              ),
+            ),
         );
       }
 }
